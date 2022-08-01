a5fd5b27f3e6c37c2b9d6019032df66a6d9034c7
mkbonini <mkbonini@gmail.com>
2022-08-01 13:51:47 -0600

    test: show page displays list of items

---
+++ spec/features/customers/show_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper' 
+
+RSpec.describe 'Customer Show Page' do 
+    it 'displays a custoemrs items' do
+        supermarket_1 = Supermarket.create!(name: 'Soopers', location: 'Denver')
+        # supermarket_2 = Supermarket.create!(name: 'Sprouts', location: 'Fort Collins')
+        customer = Customer.create!(name: 'Mike Bonini', supermarket_id: supermarket_1.id)
+        item_1 = Item.create!(name: 'pet rock', price: 1000)
+        item_2 = Item.create!(name: 'ferbie', price: 100)
+        item_3 = Item.create!(name: 'bayblade', price: 10)
+        CustomerItem.create!(customer: customer, item: item_1)
+        CustomerItem.create!(customer: customer, item: item_2)
+
+        visit "/customer/#{customer.id}"
+
+        within "#item-list" do
+            expect(page).to have_content('pet rock')
+            expect(page).to have_content('ferbie')
+            expect(page).to_not have_content('bayblade')
+        end
+    end
+
+end
