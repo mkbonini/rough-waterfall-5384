cb3a9a5996e51615daf3dd7ff06702d01f2422c6
mkbonini <mkbonini@gmail.com>
2022-08-01 14:21:19 -0600

    test: customer total price method

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe Customer, type: :model do
     it { should have_many :customer_items }
     it { should have_many(:items).through :customer_items }
   end
+
+  describe 'instance methods' do
+    it 'calculates total price' do
+      supermarket_1 = Supermarket.create!(name: 'Soopers', location: 'Denver')
+      # supermarket_2 = Supermarket.create!(name: 'Sprouts', location: 'Fort Collins')
+      customer = Customer.create!(name: 'Mike Bonini', supermarket_id: supermarket_1.id)
+      item_1 = Item.create!(name: 'pet rock', price: 1000)
+      item_2 = Item.create!(name: 'ferbie', price: 100)
+      item_3 = Item.create!(name: 'bayblade', price: 10)
+      CustomerItem.create!(customer: customer, item: item_1)
+      CustomerItem.create!(customer: customer, item: item_2)
+
+      expect(customer.total_price).to eq(1100)
+    end
+  end
 end
