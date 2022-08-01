1161ac4433d0e09b69fee347a50eb8aaf5b9523b
mkbonini <mkbonini@gmail.com>
2022-08-01 15:19:24 -0600

    feat: item index shows items from supermarket

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
     def index
-        # @supermarket = Supermarket.find(params[:id])
+        # supermarket = Supermarket.find(params[:supermarket_id])
+        @items = Item.joins(:customers)
+            .select('items.*')
+            .where(customers: {supermarket_id: params[:supermarket_id]})
+            .distinct
     end
 end
