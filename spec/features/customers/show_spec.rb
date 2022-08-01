32716ddb62457d368e70db6ea2f16529d182e03d
mkbonini <mkbonini@gmail.com>
2022-08-01 13:58:41 -0600

    test: add supermarket name to show page

--- spec/features/customers/show_spec.rb
+++ spec/features/customers/show_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper' 
 
 RSpec.describe 'Customer Show Page' do 
-    it 'displays a custoemrs items' do
+    it 'displays a custoemrs items with supermarket name' do
         supermarket_1 = Supermarket.create!(name: 'Soopers', location: 'Denver')
         # supermarket_2 = Supermarket.create!(name: 'Sprouts', location: 'Fort Collins')
         customer = Customer.create!(name: 'Mike Bonini', supermarket_id: supermarket_1.id)
@@ -14,10 +14,9 @@ RSpec.describe 'Customer Show Page' do
         visit "/customers/#{customer.id}"
 
         within "#item-list" do
-            expect(page).to have_content('pet rock')
-            expect(page).to have_content('ferbie')
+            expect(page).to have_content('Item: pet rock - Supermarket: Soopers')
+            expect(page).to have_content('Item: ferbie - Supermarket: Soopers')
             expect(page).to_not have_content('bayblade')
         end
     end
-
 end
